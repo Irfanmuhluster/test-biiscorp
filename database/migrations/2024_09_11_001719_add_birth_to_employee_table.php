cea24d5d3c83d03cf1e097aacf1acf8c1acfa99e
Muhammad Irvan <muhirfan234@gmail.com>
2024-09-11 20:46:44 +0700

    birthdate

--- database/migrations/2024_09_11_001719_add_birth_to_employee_table.php
+++ database/migrations/2024_09_11_001719_add_birth_to_employee_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employee', function (Blueprint $table) {
-            $table->dropColumn('birth');
+            $table->dropColumn('birthdate');
         });
     }
 };
